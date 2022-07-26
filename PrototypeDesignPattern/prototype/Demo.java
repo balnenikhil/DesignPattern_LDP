cfa11581bcc15e0943a96aa4fd99a4552e4ad010
balnenikhil <nikhil.chandra@zemosolabs.com>
2022-07-26 11:45:07 +0530

    added prototype Design Pattern

---
+++ PrototypeDesignPattern/prototype/Demo.java
@@ -0,0 +1,20 @@
+package prototype;
+
+public class Demo {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+        MovieShop movieShop = new MovieShop();
+        movieShop.setShopName("Marvel Movie Shop");
+        movieShop.loadData();
+
+
+        MovieShop movieShop1 =movieShop.clone();
+        movieShop.getMovies().remove(2);
+        movieShop1.setShopName("DC Movie Shop");
+
+
+        System.out.println(movieShop);
+        System.out.println(movieShop1);
+    }
+}
