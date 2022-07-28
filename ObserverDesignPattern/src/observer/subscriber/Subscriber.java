ffa9ee40b28af6a4444a2daf8eaee055d6a9274b
balnenikhil <nikhil.chandra@zemosolabs.com>
2022-07-28 11:08:08 +0530

    adding observer pattern

---
+++ ObserverDesignPattern/src/observer/subscriber/Subscriber.java
@@ -0,0 +1,18 @@
+package observer.subscriber;
+
+public class Subscriber {
+    private String  name;
+    private  Channel channel = new Channel();
+
+    public Subscriber(String name) {
+        this.name = name;
+    }
+
+    public   void  update(){
+        System.out.println("Hey "+name+" content uploaded");
+    }
+
+    public  void subscribeChannel(Channel ch){
+        channel = ch;
+    }
+}
